c0f594060ca88f4dc1ad163ce2e925704199645f
Goodguuy98 <ben.mckeever@maristdundalk.ie>
2023-10-09 23:37:48 +0100

    Refine jump, add grounded variable
    Grounded still does not detect ground as of yet.
    
    Horizontal movement will be slowed down when the player is in the air.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,11 +7,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speedGround = 10f;
+    // Fraction that ground speed will be reduced by when in air
+    public float speedFractionAir = 0.75f;
     public float jumpHeight = 10f;
+
+    public float gravityScale = 10f;
+    public float fallingGravityScale = 20f;
+
     [SerializeField] Animator animator;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] private PlayerInput playerInput;
+
+    private bool grounded = false;
 
     private void Awake() {
         animator = GetComponent<Animator>();
@@ -34,22 +42,39 @@ public class PlayerController : MonoBehaviour
         bool jump = playerInput.actions["Jump"].WasPressedThisFrame();
 
         if(jump) Jump();
+
+        // Player is rising, gravity scale is set to its typical value.
+        if(rb.velocity.y >= 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+
+        // Player is falling, gravity scale is set higher.
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallingGravityScale;
+        }
     }
 
     private void Movement() {
         //  Action from new input system
         float horizontalInput = playerInput.actions["Move"].ReadValue<Vector2>().x;
 
-        // Move the player Character
-        rb.velocity = new Vector2(horizontalInput*speed, rb.velocity.y);
-
-        animator.SetBool("Walk", horizontalInput !=0);
+        if (!grounded) {
+            // Move the player Character slower in the air
+            rb.velocity = new Vector2((horizontalInput*speedGround)*speedFractionAir, rb.velocity.y);
+        } else { // On ground, get full movement speedGround
+            rb.velocity = new Vector2(horizontalInput*speedGround, rb.velocity.y);
+            animator.SetBool("Walk", horizontalInput !=0);
+        }
     }
 
     private void Jump() {
+        grounded = true;
         //https://gamedevbeginner.com/how-to-jump-in-unity-with-or-without-physics/#:~:text=The%20basic%20method%20of%20jumping,using%20the%20Add%20Force%20method.
         // Impulse applies force immediately, creating a jumping motion
         rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.right * speedGround, ForceMode2D.Impulse);
         animator.SetTrigger("Jump");
     }
 
