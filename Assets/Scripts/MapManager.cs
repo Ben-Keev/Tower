28e02f22d4a091126ded1d290bc65210fb12b0e0
Goodguuy98 <ben.mckeever@maristdundalk.ie>
2023-10-19 02:40:45 +0100

    Add SwitchTileState
    Map Manager may now call a method to switch block tiles
    
    Currently only works with red tile map.
    
    Will utilise gamemanager to check the current state of tiles.

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class MapManager : MonoBehaviour
 {    
@@ -16,6 +18,10 @@ public class MapManager : MonoBehaviour
 
     // Link each tile to its data, making it accessible to scripts
     private Dictionary<TileBase, TileData> dataFromTiles;
+
+    public Tilemap temp;
+
+    public BoundsInt seek;
 
     private void PopulateDictionary()
     {
@@ -36,19 +42,51 @@ public class MapManager : MonoBehaviour
         {
             Debug.Log("This is a " + dataFromTiles[tile].colour + " and it is" + dataFromTiles[tile].active);
         }
+
+        SwitchTileState(Colour.Red, false);
     }
 
-
-
-    // Start is called before the first frame update
-    void Start()
+    // Switch tiles based on colour specified and what state.
+    // Use not operator for inverse
+    void SwitchTileState(Colour colour, bool active)
     {
-        
-    }
+        // TODO if statement that selects tilemap based on colour.
+        Tilemap map = temp;
+        TileBase tileNew = null;
+        TileBase tileChange = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // TODO check what blocks are out there. Check whether it's currently active or not.
+        // TODO instead keep track of these variables in game manager
+        // Set up a dummy block below the map and use its coordinates to keep track.
+        // https://docs.unity3d.com/ScriptReference/Tilemaps.Tilemap.GetTile.html
+
+        //Vector3 worldPosition = new Vector3(-3, -3, 0); // Create a world position vector
+        //Vector3Int cellPosition = temp.WorldToCell(worldPosition); // Convert world position to cell position
+
+        // TileBase tileChange = temp.GetTile(cellPosition);
+        //TileBase tileChange = tileArray[0];
+
+        // Checks the current state of whether a tile is active
+        //bool active = dataFromTiles[tileChange].active;
+
+        foreach (var tile in tiles)
+        {
+            if (dataFromTiles[tile].colour == colour && dataFromTiles[tile].active == active)
+            {
+                tileChange = tile;
+            }
+            // If the Colour matches and the active state is the opposite from the one specified
+            if (dataFromTiles[tile].colour == colour && dataFromTiles[tile].active == !active)
+            {
+                tileNew = tile;
+            }
+        }
+
+        // It should never equal null. This is to prevent errors.
+        if (tileNew != null && tileChange != null)
+        {
+            Debug.Log("Done");
+            map.SwapTile(tileChange, tileNew);
+        }
     }
 }
